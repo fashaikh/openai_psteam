198a2d9c03b19742f0bba11d1dbde34be1bdd733
ysharma21 <yachnasharma@microsoft.com>
2023-06-03 23:53:25 -0400

    bug fix

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -12,7 +12,7 @@ const EXAMPLES: ExampleModel[] = [
         text: "Load the grounding prompt",
         value: "You are an AI assistant who helps answer questions on scientific publications. \
         Use the knowledge from Google Scholar https://scholar.google.com/citations?view_op=list_works&hl=en&hl=en&tzom=300&user=elEQEEEAAAAJ \
-        to answer the questions. \"
+        to answer the questions."
     },
     { text: "Can you desribe the OSATS scoring?", value: "Can you describe the OSATS scoring?"},
     { text: "Can you provide steps for motion texture analysis?", value: "Can you provide steps for motion texture analysis?" }
